5bd0bf7d12fb84e4c87f3a02472147aa6a58bdf4
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-06-27 12:38:44 +0530

    Create Playing_with_character.c

---
+++ Playing_with_character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char c[10],s[50],sen[100];
+    gets(c);
+    gets(s);
+    gets(sen);
+    puts(c);
+    puts(s);
+    puts(sen);    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
