c6dccdf823c37ae3c542fd32069a76f686c6066b
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-07-05 21:39:02 +0530

    Create function_in_c.c

---
+++ function_in_c.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+int max_of_four(int x,int y,int z,int x1)
+{
+    if(x>y && x>z && x>x1){
+        return x;
+    }
+    else if(y>x && y>z && y>x1){
+        return y;
+    }
+    else if(z>x && z>y && z>x1){
+        return z;
+    }
+    else{
+        return x1;
+    }
+    return 0;
+}
