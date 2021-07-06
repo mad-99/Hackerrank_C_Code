61695ca045eb425a9dced8e52b788023d3c5c97c
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-07-07 00:34:31 +0530

    Create 1D_array_in_c.c

---
+++ 1D_array_in_c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a[1000],n,sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
